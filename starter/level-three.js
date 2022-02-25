f79ddbc1d57bc2c316197e531beaef354a2d3743
Michael Lackey <michael.g.lackey@gmail.com>
2022-02-25 15:07:02 -0600

    add solution

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,19 @@
  * DOTS: Level Three
  *
  */
+
+let score = 0
+let balls = document.querySelectorAll('.js-ball')
+const scoreDisplay = document.querySelector('.js-score')
+const levelWinner = document.querySelector('.level-winner')
+
+for (let i = 0; i < balls.length; i++) {
+  balls[i].addEventListener('click', () => {
+    let incrementValue = parseInt(balls[i].dataset.increment)
+    score += incrementValue
+    scoreDisplay.innerText = score
+    if (score >= 100) {
+      levelWinner.style.opacity = 1
+    }
+  }) 
+}
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,18 @@
  * DOTS: Level Two
  *
  */
+
+let score = 0
+let balls = document.querySelectorAll('.js-ball')
+const scoreDisplay = document.querySelector('.js-score')
+const levelWinner = document.querySelector('.level-winner')
+
+for (let i = 0; i < balls.length; i++) {
+  balls[i].addEventListener('click', () => {
+    score += 10
+    scoreDisplay.innerText = score
+    if (score >= 100) {
+        levelWinner.style.opacity = 1
+    }
+  }) 
+}
